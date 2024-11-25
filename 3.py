468ff2b7cbcf82d65cd30d8e9b324e8b836d5303
Lucas-023 <al.lucas.barcelos@impatech.org.br>
2024-11-25 12:20:44 -0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,10 @@
 import scipy.optimize
-def codigo(points, x0 = [1,1]):
-    def funcao(parametros, points):
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def absolute(points, x0 = [1,1]):
+    def func_abs(parametros, points):
         sum = 0
         a, b = parametros[0], parametros[1]
         for i in range (len(points)):
@@ -9,66 +13,99 @@ def codigo(points, x0 = [1,1]):
 
     
     
-    result = scipy.optimize.minimize(funcao, x0, args = (points))
-    print(f'a = {result.x[0]} and b = {result.x[1]}')
-pontos = [(1, 2), (2, 4), (3, 6), (4, 8)]
+    result = scipy.optimize.minimize(func_abs, x0, args = (points))
+    a,b = result.x
+    x = []
+    y = []
+    for i in points:
+        x.append(i[0])
+        y.append(i[1])
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'o', markeredgewidth=2)
 
-x0 = [1,1]
+    data_x = np.linspace(min(x), max(x), 100)
+    ax.plot(data_x, data_x*a + b)
 
-codigo(pontos)
+    plt.show()
 
-import numpy as np
+    return (f'a = {result.x[0]} and b = {result.x[1]}')
 
-def generate_points(m):
-    np.random.seed(1)
-    a = 6
-    b = -3
-    x = np.linspace(0, 10, m)
-    y = a*x + b + np.random.standard_cauchy(size=m)
-
-    return (x,y)
-
-def save_points(points, path = 'test_points.txt' ):
-    with open(path, 'wt') as f:
-        for x, y in zip(points[0], points[1]):
-            f.write(f'{x} {y}\n')
-
-listinha = [64, 128, 256, 512, 1024]
-def rodando(lista):
-    for i in lista:
-        c = generate_points(i)
-        c1 = []
-        for j in range(i):
-            c2 = []
-            c2.append(c[0][j])
-            c2.append(c[1][j])
-            c1.append(c2)
-        codigo(c1)
-
-rodando(listinha)
-print("\n\n\n\n")
-
-def outro_codigo(points, x0 = [1,1]):
-    def outra_funcao(parametros, points):
+def quad(points, x0 = [1,1]):
+    def func_quad(parametros, points):
         sum = 0
         a, b = parametros[0], parametros[1]
         for i in range (len(points)):
             sum+= (a*(points[i][0]) + b - points[i][1])**2
         return sum
 
-    
-    
-    result = scipy.optimize.minimize(outra_funcao, x0, args = (points))
-    print(f'a = {result.x[0]} and b = {result.x[1]}')
 
-def outro_rodando(lista):
-    for i in lista:
-        c = generate_points(i)
-        c1 = []
-        for j in range(i):
-            c2 = []
-            c2.append(c[0][j])
-            c2.append(c[1][j])
-            c1.append(c2)
-        outro_codigo(c1)
-outro_rodando(listinha)
+    result = scipy.optimize.minimize(func_quad, x0, args = (points))
+
+    a,b = result.x
+    x = []
+    y = []
+    for i in points:
+        x.append(i[0])
+        y.append(i[1])
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'o', markeredgewidth=2)
+
+    data_x = np.linspace(min(x), max(x), 100)   
+    ax.plot(data_x, data_x*a + b)
+
+    plt.show()
+
+    return(f'a = {result.x[0]} and b = {result.x[1]}')
+
+
+if __name__ == 'main':
+    pontos = [(1, 2), (2, 4), (3, 6), (4, 8)]
+
+    x0 = [1,1]
+    print(absolute(pontos))
+    print(quad(pontos))
+
+    def generate_points(m):
+        np.random.seed(1)
+        a = 6
+        b = -3
+        x = np.linspace(0, 10, m)
+        y = a*x + b + np.random.standard_cauchy(size=m)
+
+        return (x,y)
+
+    def save_points(points, path = 'test_points.txt' ):
+        with open(path, 'wt') as f:
+            for x, y in zip(points[0], points[1]):
+                f.write(f'{x} {y}\n')
+
+    listinha = [64, 128, 256, 512, 1024]
+    def rodando(lista):
+        for i in lista:
+            c = generate_points(i)
+            c1 = []
+            for j in range(i):
+                c2 = []
+                c2.append(c[0][j])
+                c2.append(c[1][j])
+                c1.append(c2)
+            print(absolute(c1))
+
+    rodando(listinha)
+    print("\n\n\n\n")
+
+
+
+    def outro_rodando(listinha):
+        for i in listinha:
+            c = generate_points(i)
+            c1 = []
+            for j in range(i):
+                c2 = []
+                c2.append(c[0][j])
+                c2.append(c[1][j])
+                c1.append(c2)
+            print(quad(c1))
+            
+    outro_rodando(listinha)
+
